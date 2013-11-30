2f39b1e615533c76858bb4968e86b772b3ee59fd
Arthur Chang <halcyonbeing@gmail.com>
2013-11-30 02:40:44 -0500

    THINGS INVOLVING ME FORGETTING TO MERGE

--- src/com/dblayout/DatabaseHelper.java
+++ src/com/dblayout/DatabaseHelper.java
@@ -1,29 +1,15 @@
 package com.dblayout;
 
-<<<<<<< HEAD
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-=======
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
-import com.Exception.RaiseYourHandError;
-import com.Exception.RaiseYourHandException;
+import java.sql.ResultSet;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 import com.dblayout.DatabaseContract.CourseEntry;
 import com.dblayout.DatabaseContract.RosterEntry;
 import com.dblayout.DatabaseContract.UserEntry;
->>>>>>> 7c93094096c6f32b88a7019addbf3229da3eb6f3
-
-
-import com.Exception.RaiseYourHandError;
-import com.Exception.RaiseYourHandException;
-import com.foo.DatabaseContract.CourseEntry;
-import com.foo.DatabaseContract.RosterEntry;
-import com.foo.DatabaseContract.UserEntry;
 
 
 /**
@@ -31,7 +17,6 @@ import com.foo.DatabaseContract.UserEntry;
  * 
  * Code based off of http://developer.android.com/training/basics/data-storage/databases.html
  */
-<<<<<<< HEAD
 public class DatabaseHelper extends SQLiteOpenHelper {
 	private static final int DATABASE_VERSION = 1;
 	private static final String DATABASE_NAME = "Reader.db";
@@ -51,70 +36,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	protected ResultSet executeQuery(String sql) {
 		ResultSet rs = null;
 		rs = stmt.executeQuery(sql);
-=======
-public class DatabaseHelper {
-
-	private static final String DATABASE_NAME = "Reader.db";
-	private Connection c = null;
-	private Statement stmt = null;
-
-	// Constructor
-	public DatabaseHelper() throws RaiseYourHandException {
-		try {
-			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_NAME);
-			c.setAutoCommit(false);
-			stmt = c.createStatement();
-		} catch ( Exception e ) {
-			throw new RaiseYourHandException(RaiseYourHandError.SQL_FAILURE, e.getMessage());
-		}
-		System.out.println("Opened database successfully");
-	}
-
-	// Creating the tables in the database
-	public void onCreate() {
-		try {
-			stmt.executeUpdate(UserEntry.SQL_CREATE_ENTRIES);
-			stmt.executeUpdate(RosterEntry.SQL_CREATE_ENTRIES);
-			stmt.executeUpdate(CourseEntry.SQL_CREATE_ENTRIES);
-		} catch (SQLException e) {
-			new RaiseYourHandException(RaiseYourHandError.SQL_FAILURE, e.getMessage());
-		}
-	}
-
-	public void onUpgrade() {
-		try {
-			stmt.executeUpdate(UserEntry.SQL_DELETE_ENTRIES);
-			stmt.executeUpdate(RosterEntry.SQL_DELETE_ENTRIES);
-			stmt.executeUpdate(CourseEntry.SQL_DELETE_ENTRIES);
-		} catch (SQLException e) {
-			new RaiseYourHandException(RaiseYourHandError.SQL_FAILURE, e.getMessage());
-		}
-		onCreate();
-	}
-
-	protected void executeUpdate(String sql) {
-		try {
-			stmt.executeUpdate(sql);
-			c.commit();
-		} catch (SQLException e) {
-			new RaiseYourHandException(RaiseYourHandError.SQL_FAILURE, e.getMessage());
-		}
-	}
-	
-	protected ResultSet executeQuery(String sql) {
-		ResultSet rs = null;
-		try {
-			rs = stmt.executeQuery(sql);
-		} catch (SQLException e) {
-			new RaiseYourHandException(RaiseYourHandError.SQL_FAILURE, e.getMessage());
-		}
->>>>>>> 7c93094096c6f32b88a7019addbf3229da3eb6f3
 		return rs;
 	}
 
 	public void onClose() {
-<<<<<<< HEAD
 	}
 
 	@Override
@@ -130,14 +55,5 @@ public class DatabaseHelper {
 		db.execSQL(RosterEntry.SQL_DELETE_ENTRIES);
 		db.execSQL(CourseEntry.SQL_DELETE_ENTRIES);		
 		onCreate(db);
-=======
-		try {
-			stmt.close();
-			c.close();
-			System.out.println("Closed database successfully");
-		} catch ( Exception e ) {
-			new RaiseYourHandException(RaiseYourHandError.SQL_FAILURE, e.getMessage());
-		}
->>>>>>> 7c93094096c6f32b88a7019addbf3229da3eb6f3
 	}
 }
